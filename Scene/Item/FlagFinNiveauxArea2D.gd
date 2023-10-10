3eafcca943a9ff809fdc476319fd9d8a73aaf4f8
2139934-Lauziere-Nathan <89462559+2139934-Lauziere-Nathan@users.noreply.github.com>
2023-10-09 21:24:20 -0400

    modif main , global

--- Scene/Item/FlagFinNiveauxArea2D.gd
+++ Scene/Item/FlagFinNiveauxArea2D.gd
@@ -1,6 +1,5 @@
 extends Area2D
-
-
+var niveaux
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,9 +7,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	niveaux = Global.niveau
 	pass
 
 
 func _on_body_entered(body):
-	get_tree().change_scene_to_file("res://Scene/Menus/MenuPrincipal.tscn")
+	print_debug("%s"%(niveaux+1))
+	Global.niveau = Global.niveau+1
+	#get_tree().change_scene_to_file("res://Scene/Level/niveaux_%s.tscn"%(niveaux+1))
 	pass # Replace with function body.
