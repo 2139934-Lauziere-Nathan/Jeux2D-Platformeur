96b730572b7041aad89ed4629addfa709a96e580
2139934-Lauziere-Nathan <89462559+2139934-Lauziere-Nathan@users.noreply.github.com>
2023-11-09 16:16:57 -0500

    petite amelioration de l'enemis, colision unpeu moin bugger, et "machine a etas" rearanger

--- Scripts/EnemisCharacterBody2D.gd
+++ Scripts/EnemisCharacterBody2D.gd
@@ -3,78 +3,144 @@ extends CharacterBody2D
 enum {
 	WALK,
 	IDLE,
-	ATTACK
+	ATTACK,
+	JUMP
 }
 
 var state = WALK
 @onready var anim = $AnimatedSprite2D
 @onready var ray2D = $RayCast2D
+@onready var ray2D2 = $RayCast2D2
 @onready var colisionBody = $CollisionShape2D
 @onready var colisionArea = $Area2D/CollisionShape2D
 
-var is_moving_left = false
-var is_moving_right = false
-var is_idle = true
 
-const SPEED = 50
-const JUMP_VELOCITY = -400.0
+
+const SPEED = 60
+const JUMP_VELOCITY = -40.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var direction = null
-var randomNumber = ceil(randf_range(0, 2))
-var randomNumberdir = ceil(randf_range(0, 2))
+var direction = 1
+
 
 func _physics_process(delta):
+	#selectIdleOrWalk()
+	#if state == WALK && $Timer.is_stopped():
+		#selectDirection()
+	selecIdleOrWalkplusDirection()
 	
-	
-	if state != ATTACK && $Timer.is_stopped():
-		print_debug(randomNumber)
-		$Timer.start()
-		if randomNumber == 1:
-			state = IDLE
-		elif randomNumber == 0:
-			state = WALK
-			if randomNumberdir == 1:
-				randomNumberdir == 1
 	etas()
 	
 	
+	velocity.y += gravity * delta
 	move_and_slide()
-var cooldown = 0
 
+func selecIdleOrWalkplusDirection():
+	var randomNumber = ceil(randf_range(0, 2))
+	var randomNumberdir = ceil(randf_range(0, 2))
+	if state != ATTACK && $Timer.is_stopped():
+
+		
+
+		if randomNumber == 1:
+			state = IDLE
+
+		elif randomNumber == 2:
+			
+
+			if randomNumberdir == 1 && $Timer.is_stopped():
+				direction = 1
+
+			elif randomNumberdir == 2 && $Timer.is_stopped():
+				direction = 2
+
+			state = WALK
+		$Timer.start()
 func etas():
 	match state:
 		IDLE:
+
 			anim.play("IDLE")
 			velocity.x = 0
 			if direction == 1:
-				anim.scale.x = 1
-				colisionArea.scale.x = 1
-				colisionBody.scale.x = 1
-			else:
-				anim.scale.x = -1  # Flip animation if moving left
-				colisionArea.scale.x = -1
-				colisionBody.scale.x = -1
+				if anim.scale.x != 1:
+					anim.scale.x = 1
+					colisionArea.position.x *= 1
+					colisionBody.position.x *= 1
+					ray2D2.position.x *= 1
+			elif direction == 2:
+				if anim.scale.x != -1:
+					anim.scale.x = -1 
+					colisionArea.position.x *= -1
+					colisionBody.position.x *= -1
+					ray2D.position.x *=-1
+					ray2D2.position.x *=-1
+				
 			
 		WALK:
+
 			anim.play("RUN")
-			velocity.x = (SPEED if direction == 1 else -SPEED)
 			detect_fall()
+			detectmur()
+			
 			if direction == 1:
-				anim.scale.x = 1
-			else:
-				anim.scale.x = -1  # Flip animation if moving left
+				velocity.x = SPEED
+				detect_fall()
+				if anim.scale.x != 1:
+					anim.scale.x = 1
+					colisionArea.position.x *= 1
+					colisionBody.position.x *= 1
+					
+					ray2D2.position.x *= 1
+			elif direction == 2:
+				velocity.x = -SPEED
+				if anim.scale.x != -1:
+					anim.scale.x = -1  # Flip animation if moving left
+					colisionArea.position.x *= -1
+					colisionBody.position.x *= -1
+					ray2D.position.x *=-1
+					
+					ray2D2.position.x *=-1
+				detectmur()
+				detect_fall()
 			
 		ATTACK:
 			anim.play("Attack")
+		JUMP:
+			anim.play("JUMP")
+			velocity.y = (1 * JUMP_VELOCITY )
+			if direction == 1:
+				velocity.x = SPEED
+			if  direction == 2:
+				velocity.x = -SPEED
+			
+			
 func detect_fall():
-	if not ray2D.is_colliding() and is_on_floor():
+	
+	if not ray2D.is_colliding():
+		print_debug("testarrettrous")
+		$Timer.start()
+		$Timer.wait_time = 1
+		state = JUMP
+		
+		#if direction == 1:
+		#	direction = 2
+		#elif direction == 2:
+		#	direction = 1
+func detectmur():
+	if ray2D2.is_colliding():
 		state = IDLE
 		velocity.x = 0
-
+		print_debug("test detection de mur")
+		$Timer.start()
+		if direction == 1:
+			direction = 2
+		elif direction == 2:
+			direction = 1
+		move_and_slide()
 
 func _on_area_2d_body_entered(body):
 	
-	print_debug("bodyentered")
+
 	if body.has_method("domage"):
 		state = ATTACK
 		body.domage()
@@ -84,5 +150,6 @@ func _on_area_2d_body_entered(body):
 
 
 func _on_animated_sprite_2d_animation_finished():
-	state = IDLE
+	if state == ATTACK || WALK:
+		state = IDLE
 	pass # Replace with function body.
