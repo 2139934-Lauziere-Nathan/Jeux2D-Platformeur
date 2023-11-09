b1e53f9dddc191ff0e464f2c11028ef7c3ed9393
2139934-Lauziere-Nathan <89462559+2139934-Lauziere-Nathan@users.noreply.github.com>
2023-11-09 09:21:33 -0500

    update enemis

--- Scripts/EnemisCharacterBody2D.gd
+++ Scripts/EnemisCharacterBody2D.gd
@@ -9,6 +9,8 @@ enum {
 var state = WALK
 @onready var anim = $AnimatedSprite2D
 @onready var ray2D = $RayCast2D
+@onready var colisionBody = $CollisionShape2D
+@onready var colisionArea = $Area2D/CollisionShape2D
 
 var is_moving_left = false
 var is_moving_right = false
@@ -17,41 +19,41 @@ var is_idle = true
 const SPEED = 50
 const JUMP_VELOCITY = -400.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var timer_idle = 500
-var timer_run = 50
 var direction = null
 var randomNumber = ceil(randf_range(0, 2))
+var randomNumberdir = ceil(randf_range(0, 2))
 
 func _physics_process(delta):
-	if timer_idle == 0:
-		state = WALK
-	else:
-		state = IDLE
-		timer_idle = timer_idle - 1
 	
-	if state == WALK:
-		timer_run = timer_run - 1
-
-		if randomNumber == 2:
-			direction = 1
-		else:
-			direction = 0
-		if timer_run == 0:
-			timer_idle = 150
-			timer_run = 50
-			randomNumber = ceil(randf_range(0, 2))
 	
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	if state != ATTACK && $Timer.is_stopped():
+		print_debug(randomNumber)
+		$Timer.start()
+		if randomNumber == 1:
+			state = IDLE
+		elif randomNumber == 0:
+			state = WALK
+			if randomNumberdir == 1:
+				randomNumberdir == 1
+	etas()
+	
+	
+	move_and_slide()
+var cooldown = 0
 
+func etas():
 	match state:
 		IDLE:
 			anim.play("IDLE")
 			velocity.x = 0
 			if direction == 1:
 				anim.scale.x = 1
+				colisionArea.scale.x = 1
+				colisionBody.scale.x = 1
 			else:
 				anim.scale.x = -1  # Flip animation if moving left
+				colisionArea.scale.x = -1
+				colisionBody.scale.x = -1
 			
 		WALK:
 			anim.play("RUN")
@@ -64,11 +66,6 @@ func _physics_process(delta):
 			
 		ATTACK:
 			anim.play("Attack")
-	
-	move_and_slide()
-var cooldown = 0
-
-	
 func detect_fall():
 	if not ray2D.is_colliding() and is_on_floor():
 		state = IDLE
@@ -76,9 +73,16 @@ func detect_fall():
 
 
 func _on_area_2d_body_entered(body):
-	state = ATTACK
+	
 	print_debug("bodyentered")
 	if body.has_method("domage"):
+		state = ATTACK
 		body.domage()
-		
+	else:
+		state = IDLE
+	pass # Replace with function body.
+
+
+func _on_animated_sprite_2d_animation_finished():
+	state = IDLE
 	pass # Replace with function body.
