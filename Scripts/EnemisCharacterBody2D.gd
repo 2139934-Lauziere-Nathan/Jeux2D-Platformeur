fa2a77cd7e2e778cc07040f5bc43cc1d90f7cdb0
2139934-Lauziere-Nathan <89462559+2139934-Lauziere-Nathan@users.noreply.github.com>
2023-11-13 12:08:52 -0500

    modif a la facon dont l'enemis reagit au terrain

--- Scripts/EnemisCharacterBody2D.gd
+++ Scripts/EnemisCharacterBody2D.gd
@@ -12,13 +12,17 @@ var state = WALK
 @onready var ray2D = $RayCast2D
 @onready var ray2D2 = $RayCast2D2
 @onready var ray2D3 = $RayCast2D3
+@onready var ray2D4 = $RayCast2D4
+@onready var TimerAgression = $TimerAgression
+@onready var Vue = $Vision
+@onready var Sol = $DetectSol
 @onready var colisionBody = $CollisionShape2D
 @onready var colisionArea = $Area2D/CollisionShape2D
 
 
 
 const SPEED = 60
-const JUMP_VELOCITY = -40.0
+const JUMP_VELOCITY = -200.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var direction = null
 
@@ -28,9 +32,9 @@ func _physics_process(delta):
 	#selectIdleOrWalk()
 	#if state == WALK && $Timer.is_stopped():
 		#selectDirection()
-	detctTomberdansUnTrous()
-	select_rand_etas()
-	
+	if $Timer.is_stopped():
+		select_rand_etas()
+	TriggerAggression()
 	etas2()
 	
 	
@@ -81,10 +85,10 @@ func handleIdleState():
 
 func handleWalkState():
 	updateCharacterDirection()
+	SauteAvantDeTomber()
+	dontKissTheWall()
 	anim.play("RUN")
-	detect_fall()
-	detectmur()
-	detectMurEtPasTrous()
+	detctTomberdansUnTrous()
 	print_debug(direction)
 	if direction == 1:
 		velocity.x = SPEED
@@ -95,15 +99,9 @@ func handleWalkState():
 	
 
 func handleJumpState():
-	if $Timer.is_stopped():
+	if detectground() == true:
 		velocity.y = JUMP_VELOCITY
-		anim.play("JUMP")
-		detectMurEtPasTrous()
-		if direction == 1:
-			velocity.x = SPEED
-		elif direction == -1:
-			velocity.x = -SPEED
-		$Timer.start()
+	
 
 func updateCharacterDirection():
 	anim.scale.x = direction
@@ -113,13 +111,7 @@ func updateCharacterDirection():
 	ray2D2.scale.x *= direction
 	ray2D3.scale.x *= direction
 
-func setCharacterDirection(scaleFactor):
-	anim.scale.x = scaleFactor
-	colisionArea.scale.x *= scaleFactor
-	colisionBody.scale.x *= scaleFactor
-	ray2D.scale.x *= scaleFactor
-	ray2D2.scale.x *= scaleFactor
-	ray2D3.scale.x *= scaleFactor
+
 			
 func detect_fall():
 	
@@ -133,32 +125,43 @@ func detect_fall():
 		#	direction = 2
 		#elif direction == 2:
 		#	direction = 1
-func detectmur():
+func detectmurAvant():
 	if ray2D2.is_colliding():
-		state = IDLE
-		velocity.x = 0
-
-		$Timer.start()
-		if direction == 1:
-			direction = -1
-		elif direction == -1:
-			direction = 1
-		updateCharacterDirection()
-		move_and_slide()
+		
 		return true
 	return false
-func detectMurEtPasTrous():
-	if ray2D2.is_colliding():
-		if not ray2D3.is_colliding():
-			state = JUMP
-			return true
+func detectground():
+	if not Sol.is_colliding():
+		return true
 	return false
+func dontKissTheWall():
+	if detectmurAvant() == true:
+		if direction != 1:
+			direction = 1
+		elif direction == 1:
+			direction = -1
+	updateCharacterDirection()
 func detctTomberdansUnTrous():
-	if ray2D.is_colliding():
-		if ray2D2.is_colliding():
+	if detectmurAvant() == true:
+		if ray2D4.is_colliding():
+			if not ray2D3.is_colliding():
+				print_debug("detctTomberdansUnTrous()")
+				$Timer.stop()
+				state = JUMP
+				$Timer.start()
+func SauteAvantDeTomber():
+	if Sol.is_colliding():
+		if not ray2D.is_colliding():
+			$Timer.stop()
 			state = JUMP
-			return true
-	return false
+			$Timer.start()
+func TriggerAggression():
+	if Vue.collide_with_bodies == true:
+		var collider = Vue.get_collider()
+		if collider and collider is CharacterBody2D:
+			state = WALK
+
+	
 func _on_area_2d_body_entered(body):
 	
 
