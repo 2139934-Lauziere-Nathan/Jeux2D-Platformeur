7a75b21ccd61569f62b5eded88ba41eb65a2c694
Max12243 <laurin.maxime1@gmail.com>
2023-11-28 08:54:53 -0500

    MODIF

--- Scripts/clé.gd
+++ Scripts/clé.gd
@@ -1,17 +1,11 @@
 extends Area2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+signal key_taken
 
 
 func _on_body_entered(body):
-	Global._plusUnToken()
+	emit_signal("key_taken")
 	queue_free()
 	pass # Replace with function body.
